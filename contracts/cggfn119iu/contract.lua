2f323c5f69e37b89444d863d70f5609077b3be44
fiatjaf <fiatjaf@alhur.es>
2021-07-11 08:48:15 -0300

    migrated contract cggfn119iu from old database

---
+++ contracts/cggfn119iu/contract.lua
@@ -0,0 +1,64 @@
+function __init__ ()
+  return {
+    ranking = {},
+    owner = '024035dc120f107d70a0ac436ec21c870d6ac1d475c4bc8c9da84e682df3395cab'
+  }
+end
+
+function sponsor_ad ()
+  local msatoshi_per_hour = call.payload.sats_per_day * 1000 / 24
+  local hours = call.msatoshi / msatoshi_per_hour
+  local seconds = hours * 60 * 60
+
+  local ad = {
+    rate = call.payload.sats_per_day,
+    msatoshi = call.msatoshi,
+    seconds = seconds,
+    placed = os.time(),
+    link = tostring(call.payload.link)
+  }
+
+  if call.payload.image_url then
+    ad.image_url = tostring(call.payload.image_url)
+  elseif call.payload.text then
+    ad.text = tostring(call.payload.text)
+  else
+    error('ad must have image or text')
+  end
+
+  -- place this ad in the ranking
+  contract.state.ranking[#contract.state.ranking + 1] = ad
+
+  -- pay the contract owner
+  contract.send(contract.state.owner, call.msatoshi)
+
+  bump()
+end
+
+function remove ()
+  if account.id ~= contract.state.owner then
+    error('only the owner can call this')
+  end
+
+  table.remove(contract.state.ranking, call.payload.index)
+end
+
+function bump ()
+  -- remove expired ads
+  for i = #contract.state.ranking, 1, -1 do
+    local ad = contract.state.ranking[i]
+    if ad.placed + ad.seconds <= os.time() then
+      table.remove(contract.state.ranking, i)
+    end
+  end
+
+  -- sort ranking
+  local function ratesorter (a, b)
+    if a.rate == b.rate then
+      return a.placed < b.placed
+    else
+      return a.rate > b.rate
+    end
+  end
+  table.sort(contract.state.ranking, ratesorter)
+end
