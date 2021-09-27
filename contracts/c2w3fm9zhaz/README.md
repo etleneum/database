f68b577344184e7ee1dfbb5efd5fe9924325ff2e
etleneum <etleneum@hulsmann>
2021-09-27 14:59:50 -0400

    contract c2w3fm9zhaz created.

---
+++ contracts/c2w3fm9zhaz/README.md
@@ -0,0 +1,9 @@
+Auction for users with Etleneum account.
+
+Create auction with starting bid (sats), min_step (sats) and auction duration (days).
+
+Place bid higher then (previous bid + minimal step) and until auction not finished.
+Previous bidder funds will be returned to his etleneum account balance.
+
+When auction time is end no one are able to place bid.
+To take funds auction owner may finish auction when auction.end_datetime time is reached.
---
+++ contracts/c2w3fm9zhaz/contract.lua
@@ -0,0 +1,56 @@
+function __init__ ()
+  return {}
+end
+
+function create_auction ()
+  if not account.id then
+    error('must be authenticated!')
+  end
+
+  local auction = {
+    auction_item = call.payload.auction_item,
+    creator_id = account.id,
+    current_top_bid = tonumber(call.payload.starting_bid)*1000,
+    min_step = tonumber(call.payload.min_step)*1000,    
+    top_bider_id = account.id,
+    end_datetime = os.time() + tonumber(call.payload.auction_duration_days) * 86400
+  }
+  contract.state[util.cuid()] = auction
+end
+
+function place_bid ()
+    if not account.id then
+        error('must be authenticated!')
+    end
+    
+    local auction_id = call.payload.auction_id
+    
+    if os.time() > contract.state[auction_id].end_datetime then
+        error("this auction is finished")
+    end
+
+    local new_bid = tonumber(call.msatoshi)
+    top_bid = contract.state[auction_id].current_top_bid
+    step = contract.state[auction_id].min_step
+    
+    if new_bid < top_bid + step then
+        error("you can only place bid higher then " .. top_bid + step .. " sats.")
+    end
+    
+    if top_bid ~= 0 then
+        contract.send(contract.state[auction_id].top_bider_id, top_bid)
+    end  
+    
+    contract.state[auction_id].current_top_bid = new_bid
+    contract.state[auction_id].top_bider_id = account.id
+end
+
+function finish_auction ()
+    local auction_id = call.payload.auction_id
+    
+    if os.time() < contract.state[auction_id].end_datetime then
+        error("this auction still not finished")
+    end
+
+    contract.send(contract.state[auction_id].creator_id, contract.state[auction_id].current_top_bid)
+end
